1a74a4efcbb6d307e80744214b37eb9e01fd5795
Smriti055 <161960307+Smriti055@users.noreply.github.com>
2025-03-03 22:26:28 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/container";
+import Container from "./container/Container";
 import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
